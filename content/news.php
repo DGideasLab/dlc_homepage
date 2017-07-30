b4c0a7ecdc671a66062753dc2d28cf2311339a70
王万霖 <dgideas@outlook.com>
2017-07-30 09:56:02 +0800

    Create new file

---
+++ content/news.php
@@ -0,0 +1,87 @@
+							<div class="media">
+								<div class="media-left">
+									<img class="media-object img-thumbnail" src="/src/mnist.png" width="80px" height="80px">
+								</div>
+								<div class="media-body">
+									<h4 class="media-heading">处理MNIST数据集：开启深度学习的大门</h4>
+									<small><strong>北京，中国</strong> 2017年07月30日</small>
+									<p>在我们发布的最新<a href="https://zhuanlan.zhihu.com/p/28190464" target="_blank">文章<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span></a>中，我们的工程师介绍了如何通过Microsoft认知工具集（CNTK）框架，实现下载并预处理<a href="http://yann.lecun.com/exdb/mnist/" target="_blank">MNIST<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span></a>数据集中的数字图像。并使用Microsoft认知工具集（CNTK）来构建不同的模型，以识别手写数字。除了传统的线性回归和前馈神经网络此外，在最近几天我们还将使用卷积神经网络（CNN）来提高手写识别的性能。</p>
+									<p>敬请关注：</p>
+									<ul>
+										<li><a href="https://zhuanlan.zhihu.com/p/28190464" target="_blank">CNTK103A：加载MNIST手写识别数据集<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span></a></li>
+										<li>CNTK103B：在MNIST数据集上进行逻辑回归<small>（尚未发布）</small></li>
+										<li>CNTK103C：在MNIST数据集上使用多层感知机<small>（尚未发布）</small></li>
+										<li>CNTK103D：在MNIST数据集上使用卷积神经网络<small>（尚未发布）</small></li>
+									</ul>
+								</div>
+							</div><hr><!--media-->
+							<div class="media">
+								<div class="media-left">
+									<img class="media-object img-thumbnail" src="/src/bootcss.png" width="80px" height="80px">
+								</div>
+								<div class="media-body">
+									<h4 class="media-heading">为移动设备重新设计的响应式布局</h4>
+									<small><strong>北京，中国</strong> 2017年07月28日</small>
+									<p>最近几天，使用移动设备进行浏览的您可能会发现，我们对DGideas亚洲研究院的官方网站进行了重新设计，使其更适合于移动设备访问。这是我们为研究院官网上线一周年所做的改进计划之一：在我们浏览网页并且获得信息的过程中，人们更多地会去关注网页的具体内容，而非额外功能。秉承这个理念，我们的工程师删除了我们认为在移动设备上不需要出现的元素：比如下载栏目，额外信息和其他冗余装饰。现在看起来，DGideas亚洲研究院的官方网站在手机上浏览更加清爽了。同时，在平板电脑和小屏幕设备上的样式也会有不同：我们对于所有屏幕尺寸的设备的表现都进行了重新设计。</p>
+									<p>事实上，在不改变网页原有结构并且对不同尺寸设备进行适配的过程，得益于<a href="//bootcss.com" target="_blank">BootStrap</a>的<strong>媒体查询</strong>功能。它能够使工程师在较短时间内，使用预定义的<strong>样式</strong>（Style），就能实现对不同屏幕尺寸设备的适应性匹配：比如，我们希望用户在手机等较小屏幕设备浏览时，不会出现桌面软件的下载链接。在传统网页的实现中，工程师通过JavaScript之类的脚本、或者PHP之类的后端语言查询用户的平台，从而针对不同的平台写出适应性的代码。而使用CSS的媒体查询，我们能够使用同一套代码来适配所有的设备。</p>
+									<p>在未来的一段时间内我们仍将持续改进我们的官方网站，并添加一些有趣的功能，及时发布最新消息。</p>
+								</div>
+							</div><hr><!--media-->
+							<div class="media">
+								<div class="media-left">
+									<img class="media-object img-thumbnail" src="/src/raspberry.png" width="80px" height="64px">
+								</div>
+								<div class="media-body">
+									<h4 class="media-heading">DGideas亚洲研究院北京节点成立一周年</h4>
+									<small><strong>北京，中国</strong> 2017年07月25日</small>
+									<p>我们很高兴地宣布DGideas亚洲研究院北京节点成立一周年。2016年07月26日，我们正式将一台树莓派3配置并部署为一个多功能服务器，并且配置到公网运行，这成为了我们的北京节点。在正式运行的一年期间，DGideas亚洲研究院北京节点在应用和科研方面做出和很多贡献。我们使用北京节点进行<strong>计算机视觉</strong>（Computer Vision），<strong>计算机图形学</strong>（Computer Grahics）与<strong>机器学习</strong>（Machine Learning）方面的研究。</p>
+									<p>根据近一年来的监控和观察，DGideas亚洲研究院北京节点运行情况良好，负载较低。未来的一段时间，DGideas亚洲研究院将与教育科研机构合作，发布一些激动人心的内容。我们将根据北京节点的具体负荷，来决定我们是否升级硬件配置。</p>
+								</div>
+							</div><hr><!--media-->
+							<div class="media">
+								<div class="media-left">
+									<img class="media-object img-thumbnail" src="/src/cntk.png" width="80px" height="80px">
+								</div>
+								<div class="media-body">
+									<h4 class="media-heading">发布CNTK认知工具集讲解</h4>
+									<small><strong>北京，中国</strong> 2017年07月22日</small>
+									<p>2017年07月22日，DGideas亚洲研究院在知乎开通了CNTK认知工具集的专栏。这个专栏将向用户讲解微软认知工具集（CNTK）的原理、方法以及应用：</p>
+									<p>2016年1月25日，微软公司在他们的GitHub仓库上宣布开源了由微软研究院开发的计算网络工具集CNTK。早在2014年，在微软公司内部，黄学东博士和他的团队正在对计算机能够理解语音的能力进行更快的改进，但当时使用的工具显然拖慢了他们的进度。于是，一组由志愿者组成的开发团队构想设计了他们自己的解决方案，这项努力得到了回报。自从2016年1月微软公司宣布开源自家的认知工具集CNTK之后，在这一年多的时间里，经过开发者和开源社区的不断努力，已经取得了巨大的进步：从支持Python语言接口，到发布了工具集的2.0版本……CNTK工具正在在社区的推动下快速地发展着。CNTK 是一个统一的计算网络框架，它将深层神经网络描述为一系列通过有向图的计算步骤。在有向图中，每个节点代表一个输入值或一个网络参数，每个边表示在其中的一个矩阵运算。CNTK 提供了实现前向计算和梯度计算的算法。CNTK中预定义了很多主流的计算网络结构，用户可以轻松地在开源许可证下扩展节点类型。社区可以利用它来更方便地来推进关于人工智能的研究……</p>
+									<a href="https://zhuanlan.zhihu.com/msrcntk" class="btn btn-default btn-sm" role="button" target="_blank">继续阅读</a>
+									<small>外部链接，发布至<a href="//zhihu.com" target="_blank">知乎<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span></a>.</small>
+								</div>
+							</div><hr><!--media-->
+							<div class="media">
+								<div class="media-left">
+									<img class="media-object img-thumbnail" src="/src/tsinghua.png" width="80px" height="80px">
+								</div>
+								<div class="media-body">
+									<h4 class="media-heading">DGideas亚洲研究院与清华大学计算机组成原理实验室进行交流</h4>
+									<small><strong>北京，中国</strong> 2017年05月16日</small>
+									<p>在最近的一个学期内，我们参加了清华大学计算机组成原理实验室李山山老师的相关课程。在老师的指导下，我们从基础的逻辑电路开始，通过循序渐进的步骤，最终设计出了基于THCO-MIPS指令集的处理器。在参加课程期间，我们与老师进行了充分的交流，并对我们的成果进行了积极展示。</p>
+									<p>DGideas亚洲研究院希望将高品质的内容和相关知识回馈给用户。我们在前一段时间开通了专门介绍使用<strong>超高速电路设计语言</strong>（VHDL）对于<strong>可编程逻辑器件</strong>（FPGA）的设计教程。我们希望通过这样一种方式，让更多人有机会能学习到相关的知识。</p>
+								</div>
+							</div><hr><!--media-->
+							<div class="media">
+								<div class="media-left">
+									<img class="media-object img-thumbnail" src="/src/fpga.jpg" width="80px" height="80px">
+								</div>
+								<div class="media-body">
+									<h4 class="media-heading">你好，FPGA！</h4>
+									<small><strong>北京，中国</strong> 2017年03月27日</small>
+									<p>最近，DGideas亚洲研究院在知乎开通了有关FPAG设计的专栏，这个专栏主要向用户讲解如何使用<strong>超高速电路设计语言</strong>（VHDL），从开发简单的计时器到设计出自己的处理器：</p>
+									<p>FPGA（Field-Programmable Gate Array），现场可编程逻辑门阵列，是一种在制造后可以被用户编程修改的电路。它不像我们在实验室中常见的电阻、三级管和电容器，只能完成固定的单一功能——FPGA可以通过硬件描述语言（Hardware Description Language，HDL）来进行编程，从而完成某种特定任务。VHDL（超高速集成电路硬件描述语言，VHSIC Hardware Description Language）是其中一种被广泛使用的HDL语言。另外一种是Verilog HDL，在工业界也很流行……</p>
+									<a href="https://zhuanlan.zhihu.com/p/26030792" class="btn btn-default btn-sm" role="button" target="_blank">继续阅读</a>
+									<small>外部链接，发布至<a href="//zhihu.com" target="_blank">知乎<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span></a>.</small>
+								</div>
+							</div><hr><!--media-->
+							<div class="media">
+								<div class="media-left">
+									<img class="media-object img-thumbnail" src="/src/fly.png" width="80px" height="80px">
+								</div>
+								<div class="media-body">
+									<h4 class="media-heading">DGideas亚洲研究院转发服务器上线</h4>
+									<small><strong>洛杉矶，美国</strong> 2017年03月03日</small>
+									<p>在最近的一段时间，我们的工程师在美国洛杉矶架设了我们的美国节点服务器，用于数据存储，清洗，分析和转发。由于科研和交流需要，DGideas亚洲研究院进行的部分项目的数据需要从外国网站获得，架设美国节点有助于在美国本土预先对数据进行清理和分析，再将数据传输。这有助于减少我们在北京节点的负荷，并提高我们的工作效率。</p>
+								</div>
+							</div><hr><!--media-->
